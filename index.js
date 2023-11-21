7fac9cedac0daed49f96e2c0835a3cad5e3a595e
Gultaj Muradova 14125 <gultajmuradova14125@ada.edu.az>
2023-11-21 15:27:17 +0400

    add week 5 taks 1

---
+++ index.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "HA HA I CHANGED IT";
+  }
+
+  let firstName = 'gultaj';
+  let lastName = 'muradova';
+
+  console.log(firstName + ' ' + lastName);
+
+  const pi = 3.14;
+  console.log(pi);
--- script.js
+++
@@ -1,3 +0,0 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "HA HA I CHANGED IT";
-  }
